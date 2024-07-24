ba2d2a88e541a19e7c55cfa4a6ba2dc635cb2838
Juan Pablo Davalos <davjuanpablo423@gmail.com>
2024-07-24 13:26:08 -0700

    connecting frontend and backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
+import { useEffect, useState } from "react";
+
 export default function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    async function getTodos() {
+      const res = await fetch("/api/todos");
+      const todos = await res.json();
+
+      setMessage(todos.mssg);
+    }
+    getTodos();
+  }, [])
+  
   return (
     <main className="container">
       <h1>Todo App</h1>
+      {message && <p>{message}</p>}
     </main>
   );
 }
